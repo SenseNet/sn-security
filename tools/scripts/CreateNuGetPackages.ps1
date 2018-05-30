01dfb8d0065e714db9a8322ccd8f651061667164
tusmester <tusmester@gmail.com>
2018-05-30 09:57:29 +0200

    Raise EF6 store project version to 2.3.1 and consolidate nuget package.

--- tools/scripts/CreateNuGetPackages.ps1
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -1,5 +1,8 @@
 ﻿$srcPath = [System.IO.Path]::GetFullPath(($PSScriptRoot + '\..\..\src'))
 
+# delete existing packages
+Remove-Item $PSScriptRoot\*.nupkg
+
 nuget pack $srcPath\SenseNet.Security\SenseNet.Security.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 nuget pack $srcPath\SenseNet.Security.EF6SecurityStore\SenseNet.Security.EF6SecurityStore.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 nuget pack $srcPath\SenseNet.Security.Messaging.Msmq\SenseNet.Security.Messaging.Msmq.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
