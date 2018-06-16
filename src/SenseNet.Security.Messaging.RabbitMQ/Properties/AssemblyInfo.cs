4451d8ad589e612a6f4270690b669743539f597c
tusmester <tusmester@gmail.com>
2018-06-16 17:39:42 +0200

    Convert RabbitMQ messaging project to .net standard.

--- src/SenseNet.Security.Messaging.RabbitMQ/Properties/AssemblyInfo.cs
+++ src/SenseNet.Security.Messaging.RabbitMQ/Properties/AssemblyInfo.cs
@@ -2,23 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-#if DEBUG
-[assembly: AssemblyTitle("SenseNet.Security.Messaging.RabbitMQ (Debug)")]
-#else
-[assembly: AssemblyTitle("SenseNet.Security.Messaging.RabbitMQ (Release)")]
-#endif
-
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Sense/Net Inc.")]
-[assembly: AssemblyCopyright("Copyright © Sense/Net Inc.")]
-[assembly: AssemblyProduct("SenseNet.Security.Messaging.RabbitMQ")]
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
 
 [assembly: ComVisible(false)]
-[assembly: Guid("db2230a3-478e-4be3-b6ce-d4accd833bba")]
--- tools/scripts/CreateNuGetPackages.ps1
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -5,5 +5,4 @@ Remove-Item $PSScriptRoot\*.nupkg
 
 nuget pack $srcPath\SenseNet.Security.EF6SecurityStore\SenseNet.Security.EF6SecurityStore.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 nuget pack $srcPath\SenseNet.Security.Messaging.Msmq\SenseNet.Security.Messaging.Msmq.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
-nuget pack $srcPath\SenseNet.Security.Messaging.RabbitMQ\SenseNet.Security.Messaging.RabbitMQ.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 
