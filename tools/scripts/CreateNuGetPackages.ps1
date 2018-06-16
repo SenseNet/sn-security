7e9358346ac79327424793909d5a9e21dae2268c
tusmester <tusmester@gmail.com>
2018-06-16 14:30:13 +0200

    Convert main Security project to netstandard. Tests are green.

--- tools/scripts/CreateNuGetPackages.ps1
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -3,7 +3,6 @@
 # delete existing packages
 Remove-Item $PSScriptRoot\*.nupkg
 
-nuget pack $srcPath\SenseNet.Security\SenseNet.Security.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 nuget pack $srcPath\SenseNet.Security.EF6SecurityStore\SenseNet.Security.EF6SecurityStore.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 nuget pack $srcPath\SenseNet.Security.Messaging.Msmq\SenseNet.Security.Messaging.Msmq.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 nuget pack $srcPath\SenseNet.Security.Messaging.RabbitMQ\SenseNet.Security.Messaging.RabbitMQ.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
