0956f79fce2c11bbce553ec7a49191f45844723d
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-10-04 06:21:28 +0200

    Add more sharing tests.

---
+++ src/SenseNet.Security.Tests/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenseNet.Security.Tests
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Shortcut method for test. Example usage: aclEd.Allow("E2", "U1", false, "___________+___");
+        /// Bitmask accepts two kind of characters: '_' means active, any other means inactive.
+        /// </summary>
+        /// <returns></returns>
+        public static AclEditor Allow(this AclEditor aclEd, string entity, string identity, string bitMask, bool localOnly = false)
+        {
+            var perms = GetPermissionTypes(bitMask);
+            aclEd.Allow(Tools.GetId(entity), Tools.GetId(identity), localOnly, perms);
+            return aclEd;
+        }
+
+        private static PermissionTypeBase[] GetPermissionTypes(string bitMask)
+        {
+            var result = new List<PermissionTypeBase>();
+            var length = bitMask.Length;
+
+            for (var i = 0; i < length; i++)
+            {
+                if(bitMask[i]!='_')
+                    result.Add(PermissionTypeBase.GetPermissionTypeByIndex(length - i - 1));
+            }
+
+            return result.ToArray();
+        }
+    }
+}
--- src/SenseNet.Security.Tests/TestCasesSharingTests.cs
+++ src/SenseNet.Security.Tests/TestCasesSharingTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.Security.Data;
 using SenseNet.Security.Tests.TestPortal;
 
 namespace SenseNet.Security.Tests
@@ -128,8 +130,9 @@ namespace SenseNet.Security.Tests
             Assert.IsFalse(ctx.HasPermission(Id("E2"), PermissionType.See));
             Assert.IsTrue(ctx.HasPermission(Id("E2"), PermissionType.Preview));
         }
+
         [TestMethod]
-        public void Sharing_Eval_BreakOperation_SharingNotCopied()
+        public void Sharing_Acl_BreakOperation_SharingNotCopied()
         {
             EnsureRepository();
             var ctx = CurrentContext.Security;
@@ -140,18 +143,126 @@ namespace SenseNet.Security.Tests
             SetAcl("+E2| Normal|+G1:_____________+_");
             SetAcl("+E3| Normal|+G2:_____________+_");
 
-            // Action
+            // ACTION
             var ed = ctx.CreateAclEditor();
             ed.BreakInheritance(Id("E5"));
             ed.BreakInheritance(Id("E8"));
             ed.Apply();
 
-            // Assert
+            // ASSERT
             var acl14 = ctx.GetAcl(Id("E14")); // E1/E2/E5/E14
             var acl20 = ctx.GetAcl(Id("E20")); // E1/E3/E8/E20
 
+            // original:     +E14|Normal|+G1:______________________________________________________________++
             Assert.AreEqual("+E14|Normal|+G1:______________________________________________________________+_", Tools.ReplaceIds(acl14.ToString()));
+
+            // original:    <+E20|Normal|+G1:_______________________________________________________________+,
+            //                    Normal|+G2:______________________________________________________________+_
             Assert.AreEqual("+E20|Normal|+G2:______________________________________________________________+_", Tools.ReplaceIds(acl20.ToString()));
+        }
+        [TestMethod]
+        public void Sharing_Acl_ExplicitEntries_NotMerged()
+        {
+            EnsureRepository();
+            var ctx = CurrentContext.Security;
+
+            Tools.SetMembership(ctx, "U1:G1");
+
+            // ACTION 1
+            SetAcl("+E1| Normal|+G1:_____________++");
+
+            // ASSERT 1
+            var storedAces = ctx.DataProvider.LoadAllAces().ToArray();
+            Assert.AreEqual(1, storedAces.Length);
+            Assert.AreEqual(EntryType.Normal, storedAces[0].EntryType);
+            Assert.AreEqual(0x3ul, storedAces[0].AllowBits);
+
+            var entries = ctx.GetExplicitEntries(Id("E1"));
+            Assert.AreEqual(1, entries.Count);
+            Assert.AreEqual(0x3ul, entries[0].AllowBits);
+            Assert.AreEqual(EntryType.Normal, entries[0].EntryType);
+
+            // ACTION 2
+            SetAcl("+E1|Sharing|+G1:___________++__");
+
+            // ASSERT 2
+            storedAces = ctx.DataProvider.LoadAllAces().ToArray();
+            Assert.AreEqual(2, storedAces.Length);
+            Assert.AreEqual(0x3ul, storedAces[0].AllowBits);
+            Assert.AreEqual(EntryType.Normal, storedAces[0].EntryType);
+            Assert.AreEqual(0xCul, storedAces[1].AllowBits);
+            Assert.AreEqual(EntryType.Sharing, storedAces[1].EntryType);
+
+            entries = ctx.GetExplicitEntries(Id("E1"));
+            Assert.AreEqual(2, entries.Count);
+            Assert.AreEqual(0x3ul, entries[0].AllowBits);
+            Assert.AreEqual(EntryType.Normal, entries[0].EntryType);
+            Assert.AreEqual(0xCul, entries[1].AllowBits);
+            Assert.AreEqual(EntryType.Sharing, entries[1].EntryType);
+        }
+        [TestMethod]
+        public void Sharing_Acl_EffectiveEntries_MergedWell()
+        {
+            EnsureRepository();
+            var ctx = CurrentContext.Security;
+
+            Tools.SetMembership(ctx, "U1:G1");
+
+            // ACTION 1
+            SetAcl("+E1| Normal|+G1:______________+");
+            SetAcl("+E2| Normal|+G1:_____________+_");
+
+            // ASSERT 1
+            var entries = ctx.GetEffectiveEntries(Id("E2"));
+            Assert.AreEqual(1, entries.Count);
+            Assert.AreEqual(0x3ul, entries[0].AllowBits);
+            Assert.AreEqual(EntryType.Normal, entries[0].EntryType);
+
+            // ACTION 2
+            SetAcl("+E1|Sharing|+G1:____________+__");
+
+            // ASSERT 2
+            Assert.AreEqual(2, entries.Count);
+            Assert.AreEqual(0x3ul, entries[0].AllowBits);
+            Assert.AreEqual(EntryType.Normal, entries[0].EntryType);
+            Assert.AreEqual(0x8ul, entries[1].AllowBits);
+            Assert.AreEqual(EntryType.Sharing, entries[0].EntryType);
+
+            // ACTION 3
+            SetAcl("+E2|Sharing|+G1:___________+___");
+
+            // ASSERT 3
+            Assert.AreEqual(2, entries.Count);
+            Assert.AreEqual(0x3ul, entries[0].AllowBits);
+            Assert.AreEqual(EntryType.Normal, entries[0].EntryType);
+            Assert.AreEqual(0xCul, entries[1].AllowBits);
+            Assert.AreEqual(EntryType.Sharing, entries[0].EntryType);
+        }
+
+        [TestMethod]
+        public void Sharing_AclEd_DefaultDoesNotSeeSharingEntries()
+        {
+            EnsureRepository();
+            var ctx = CurrentContext.Security;
+
+            Tools.SetMembership(ctx, "U1:G1");
+
+            SetAcl("+E1| Normal|+G1:______________+");
+            SetAcl("+E2| Normal|+G1:_____________+_");
+            SetAcl("+E2|Sharing|+G1:___________+___");
+
+            // ACTION
+            var ed = ctx.CreateAclEditor();
+            ed.Allow("E2", "G1", "____________+__");
+            ed.Apply();
+
+            // ASSERT
+            Assert.Inconclusive();
+
+            var acl = ctx.GetAcl(Id("E2"));
+            Assert.AreEqual("+E2|Normal|+G1:_____________________________________________________________+_+", Tools.ReplaceIds(acl.ToString()));
+
+            PermissionTypeBase.InferForcedRelations();
         }
     }
 }
