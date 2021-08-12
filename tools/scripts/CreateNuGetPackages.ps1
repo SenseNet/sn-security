e1183e93015b635f9d94784ad7c7cce5bb373385
tusmester <tusmester@gmail.com>
2021-08-12 22:36:36 +0200

    Remove old EF6 package creator step from the ps script.

--- tools/scripts/CreateNuGetPackages.ps1
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -3,6 +3,5 @@
 # delete existing packages
 Remove-Item $PSScriptRoot\*.nupkg
 
-nuget pack $srcPath\SenseNet.Security.EF6SecurityStore\SenseNet.Security.EF6SecurityStore.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 nuget pack $srcPath\SenseNet.Security.Messaging.Msmq\SenseNet.Security.Messaging.Msmq.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 
