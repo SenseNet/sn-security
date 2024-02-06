612d924ad5c86b12319b2ecd51bebb4241aa5a55
Zoltán Gyebrovszki <kavics@sensenet.com>
2024-02-06 02:05:45 +0100

    ReloadCacheActivity

---
+++ src/SenseNet.Security/Messaging/SecurityMessages/ReloadCacheActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SenseNet.Security.Messaging.SecurityMessages;
+
+/// <summary>
+/// Represents an activity that reloads the security cache after any other active security activity is finished activity.
+/// </summary>
+[Serializable]
+public class ReloadCacheActivity : SecurityActivity
+{
+    protected override Task StoreAsync(SecurityContext context, CancellationToken cancel)
+    {
+        // do nothing
+        return Task.CompletedTask;
+    }
+
+    protected override void Apply(SecurityContext context)
+    {
+        context.SecuritySystem.Cache.Reset();
+    }
+
+    internal override bool ShouldWaitFor(SecurityActivity olderActivity)
+    {
+        return true;
+    }
+}
